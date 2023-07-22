255b5bf51f81c0d7a3f939df5189e7149f16c9a2
João Paulo Hora <joaopaulo8hora@gmail.com>
2023-07-22 05:48:27 -0300

    Update AcimaDiagonalSecundaria.java

--- DesafiosAritmeticos/AcimaDiagonalSecundaria.java
+++ DesafiosAritmeticos/AcimaDiagonalSecundaria.java
@@ -1,1 +1,35 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
+public class AcimaDiagonalSecundaria {
+  public static void main(String[] args) {
+      Random random = new Random();
+      Scanner leitor = new Scanner(System.in);
+      ArrayList<Double> lista = new ArrayList<>();
+      char O = leitor.next().toUpperCase().charAt(0);
+      Double[][] G = new Double[12][12];
+      for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 12; j++) {  
+            G[i][j] = random.nextDouble(10);       
+            lista.add(G[i][j]);
+        }
+      }
+      System.out.println();
+      double soma = 0;
+      double media;
+      for(int c = 0; c < lista.size(); c++) {
+        soma += lista.get(c);
+      }
+      if (O == 'S') {
+        System.out.printf("%.1f", soma);
+      } else if(O == 'M') {
+        media = soma/lista.size();
+        System.out.printf("%.1f", media);
+      } else {
+        System.out.println("Impossível realizar a operação se não de soma(S) ou média(M)");
+      }
+
+      leitor.close();
+  }
+}
